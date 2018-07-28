b12b399a0b88db39ad7213678b2cb934a596e906
lexa <sokolov@ozero.net>
2018-07-29 01:59:58 +0300

    Made firs query

--- app/src/main/java/net/ozero/wallpapersandroiddev/SearchResultActivity.java
+++ app/src/main/java/net/ozero/wallpapersandroiddev/SearchResultActivity.java
@@ -4,18 +4,53 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import net.ozero.wallpapersandroiddev.rest.model.Hit;
+import net.ozero.wallpapersandroiddev.rest.model.Result;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SearchResultActivity extends AppCompatActivity {
+
+    private ArrayList<Hit> hits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
 
+        hits = new ArrayList<>();
         RecyclerView recyclerView = findViewById(R.id.rvSearchResult);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        final RVAdapter rvAdapter = new RVAdapter(this, hits);
+        recyclerView.setAdapter(rvAdapter);
+
+        App.getRestClient().getApi()
+                .search("yellow", "photo")
+                .enqueue(new Callback<Result>() {
+                    @Override
+                    public void onResponse(Call<Result> call, Response<Result> response) {
+                        if (response.isSuccessful()) {
+                            Result result = response.body();
+                            if (result.getTotalHits() > 0) {
+                                rvAdapter.addData(result.getHits());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Result> call, Throwable t) {
+
+                    }
+                });
 
         
     }
