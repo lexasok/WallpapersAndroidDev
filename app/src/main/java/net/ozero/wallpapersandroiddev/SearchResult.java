8b3c7c8fea0f05fa3033eba02cba8d87d672f37e
lexa <sokolov@ozero.net>
2018-07-28 23:49:30 +0300

    recyclerView initialized

--- app/src/main/java/net/ozero/wallpapersandroiddev/SearchResult.java
+++ app/src/main/java/net/ozero/wallpapersandroiddev/SearchResult.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -13,6 +14,9 @@ public class SearchResult extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+
+        RecyclerView recyclerView = findViewById(R.id.rvSearchResult);
+
         
     }
 
