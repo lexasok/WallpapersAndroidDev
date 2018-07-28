9e1714aad81aaa83d71819232b49974d5a6bcbbb
lexa <sokolov@ozero.net>
2018-07-29 01:31:14 +0300

    Adapter declared

---
+++ app/src/main/java/net/ozero/wallpapersandroiddev/RVAdapter.java
@@ -0,0 +1,32 @@
+package net.ozero.wallpapersandroiddev;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder>{
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
