7a379065112508c84d887f2c3315bcf21ebb0a30
lexa <sokolov@ozero.net>
2018-07-28 23:40:42 +0300

    Rest client implemented

--- app/src/main/java/net/ozero/wallpapersandroiddev/App.java
+++ app/src/main/java/net/ozero/wallpapersandroiddev/App.java
@@ -2,7 +2,16 @@ package net.ozero.wallpapersandroiddev;
 
 import android.app.Application;
 
+import net.ozero.wallpapersandroiddev.rest.RestClient;
+
 public class App extends Application {
 
+    private static RestClient restClient;
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        restClient = new RestClient();
+    }
 }
--- app/src/main/java/net/ozero/wallpapersandroiddev/rest/RestClient.java
+++ app/src/main/java/net/ozero/wallpapersandroiddev/rest/RestClient.java
@@ -1,4 +1,24 @@
 package net.ozero.wallpapersandroiddev.rest;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RestClient {
+
+    private static final String BASE_URL = "https://pixabay.com/api/";
+    private Api mApi;
+
+    public RestClient() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        mApi = retrofit.create(Api.class);
+    }
+
+    public Api getApi() {
+        return mApi;
+    }
 }
