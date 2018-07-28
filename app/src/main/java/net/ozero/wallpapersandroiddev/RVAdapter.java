eae4378a22639ea5b8f73e82832c5f10ac9674de
lexa <sokolov@ozero.net>
2018-07-29 01:41:09 +0300

    Lib added, adapter initialized

--- app/src/main/java/net/ozero/wallpapersandroiddev/RVAdapter.java
+++ app/src/main/java/net/ozero/wallpapersandroiddev/RVAdapter.java
@@ -1,32 +1,64 @@
 package net.ozero.wallpapersandroiddev;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import net.ozero.wallpapersandroiddev.rest.model.Hit;
+import net.ozero.wallpapersandroiddev.rest.model.Result;
+
+import java.util.ArrayList;
 
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder>{
+
+    private Context mContext;
+    private ArrayList<Hit> mData;
+
+    public RVAdapter(Context context, ArrayList<Hit> hits) {
+        mContext = context;
+        mData = hits;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(
+                parent.getContext()
+        ).inflate(R.layout.rv_row, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        ImageView imageView = holder.imageView;
+        Glide
+                .with(mContext)
+                .load(mData.get(position).getWebformatURL())
+                .into(imageView);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        private ImageView imageView;
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            imageView = itemView.findViewById(R.id.rvImageView);
         }
     }
 }
