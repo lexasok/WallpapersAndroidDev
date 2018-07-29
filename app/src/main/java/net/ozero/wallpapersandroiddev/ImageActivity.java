e04079fafa76483d5e8a042c49837ea02f300a04
lexa <sokolov@ozero.net>
2018-07-30 01:40:33 +0300

    Wallpaper setting made

--- app/src/main/java/net/ozero/wallpapersandroiddev/ImageActivity.java
+++ app/src/main/java/net/ozero/wallpapersandroiddev/ImageActivity.java
@@ -1,12 +1,29 @@
 package net.ozero.wallpapersandroiddev;
 
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Looper;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.ExecutionException;
 
 public class ImageActivity extends AppCompatActivity {
 
@@ -42,7 +59,21 @@ public class ImageActivity extends AppCompatActivity {
             setWallpaperButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+                    Glide.with(getApplicationContext())
+                            .asBitmap()
+                            .load(wallpaperURL)
+                            .into(new SimpleTarget<Bitmap>() {
+                                @Override
+                                public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
+                                    WallpaperManager wallpaperManager
+                                            = WallpaperManager.getInstance(getApplicationContext());
+                                    try {
+                                        wallpaperManager.setBitmap(resource);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
                 }
             });
 
